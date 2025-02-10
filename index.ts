33231e9e29c6cd430ad59477809b316b9f25a62f
nickjmoss <nick.moss000@gmail.com>
2025-02-10 11:07:49 -0700

    remove

--- index.ts
+++
@@ -1,5 +0,0 @@
-import { peerDependencies } from './package.json';
-const peerString = Object.keys(peerDependencies)
-    .map((key) => `${key}`)
-    .join(' ');
-console.log(`bun install ${peerString}`);
